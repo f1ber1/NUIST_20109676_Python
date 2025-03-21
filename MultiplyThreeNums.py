1f3e80a6ead5e02830cdcc127a28eae9db03b2c9
f1ber1 <976559217a@gmail.com>
2025-03-21 03:25:08 -0900

    Added MultiplyThreeNums.py

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,7 @@
+num1 = int(input("please enter first number:"))
+num2 = int(input("please enter second number:"))
+num3 = int(input("please enter third number:"))
+
+res = num1 * num2 * num3
+
+print(res)
